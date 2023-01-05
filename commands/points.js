4b3879745413c2faa8afe653c470cd9b1240ae28
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-01-05 15:58:45 +0900

    포인트 랭킹 추가, 봇 감지
    유저 데이터베이스에 봇은 추가할 수 없도록 패치
    서버 포인트 랭킹 커맨드 추가

--- commands/points.js
+++ commands/points.js
@@ -14,16 +14,20 @@ module.exports = {
                 .addUserOption(option => option.setName('대상')
                     .setDescription('기부하실 대상을 선택해주세요.').setRequired(true))
                 .addIntegerOption(option => option.setName('포인트')
-                    .setDescription('기부하실 포인트를 입력해주세요.').setRequired(true))),
+                    .setDescription('기부하실 포인트를 입력해주세요.').setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand.setName('랭킹')
+                .setDescription('서버 포인트 랭킹을 10위 까지 확인하실 수 있어요.')),
     /**
      * 
      * @param {Discord.Client} client 
      * @param {Discord.Interaction} interaction 
      */
     async execute(client, interaction) {
+        let isRow = false;
         const user = interaction.user;
         const embed = new Discord.EmbedBuilder();
-
+        const row = new Discord.ActionRowBuilder();
         const file = fs.readFileSync('./data/users.json', 'utf-8');
         const users = JSON.parse(file);
 
@@ -34,8 +38,7 @@ module.exports = {
         // 기부
         switch (interaction.options.getSubcommand()) {
             case '보유':
-                let points = 0;
-                points = users[user.id].points;
+                users[user.id].name = user.username;
                 embed.setTitle(`${user.username}님의 포인트`);
                 embed.setDescription(`${users[user.id].points}포인트 보유 중`);
                 embed.setColor(0x1FF0B2);
@@ -47,7 +50,9 @@ module.exports = {
                 embed.setTitle('기부가 불가해요!');
                 embed.setColor(0xFF0000);
 
-                if (users[user.id].points == 0 || users[user.id].points < donation) {
+                if (target.bot) {
+                    embed.setDescription('봇에게 포인트를 기부하실 수 없어요!');
+                } else if (users[user.id].points == 0 || users[user.id].points < donation) {
                     embed.setDescription(`${user.username}님이 보유하신 포인트가 기부하실 포인트보다 부족해요!`);
                 } else {
                     if (users[target.id] == undefined) {
@@ -62,9 +67,32 @@ module.exports = {
                     embed.addFields({ name: `${user.username}님의 잔여 포인트`, value: `${users[user.id].points}` });
                     embed.setColor(0x1FF0B2);
                 }
+                break;
+            case '랭킹':
+                const guild = interaction.guild;
+                let serverUserList = [];
+                let desc = '';
+                embed.setTitle(`${guild.name} 포인트 랭킹`);
 
-                users[user.id].name = user.username;
-                users[target.id].name = target.username;
+                (await interaction.guild.members.fetch()).each(member => {
+                    if (member.user.bot) return;
+                    if (users[member.user.id] == undefined) {
+                        users[member.user.id] = { name: member.user.username, points: 0, attendance: "0", attendance_count: 0 };
+                    }
+                    serverUserList.push({ username: member.displayName, points: users[member.user.id].points });
+                });
+
+                serverUserList.sort((a, b) => {
+                    if (a.points < b.points) return 1;
+                    if (a.points > b.points) return -1;
+                    return 0;
+                });
+
+                for (let i = 0; i < 10; i++) {
+                    if (serverUserList.length <= i) break;
+                    embed.addFields({ name: `${i + 1}위: ${serverUserList[i].username}`, value: `${serverUserList[i].points}포인트`, inline: false });
+                }
+                embed.setColor(0x1FF0B2);
                 break;
         }
 
